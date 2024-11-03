c299df9b5a989535f5b545e1edc5a02222f75758
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-11-03 18:41:43 +0530

    Update index.js

--- src/components/MatchCard/index.js
+++ src/components/MatchCard/index.js
@@ -1,25 +1,24 @@
-// Write your code here
 import './index.css'
 
 const MatchCard = props => {
-  const {recentMatchData} = props
-  const {
-    result,
-    competingTeam,
-    competingTeamLogo,
+  const {matchDetails} = props
+  const {competingTeamLogo, competingTeam, matchStatus, result} = matchDetails
+  const getMatchStatusClassName = status =>
+    status === 'Won' ? 'match-won' : 'match-lost'
+  const matchStatusClassName = `match-status ${getMatchStatusClassName(
     matchStatus,
-  } = recentMatchData
-  const statusColor = matchStatus === 'Won' ? 'won-status' : 'lost-status'
+  )}`
+
   return (
-    <li className="recent-teams-card">
+    <li className="match-item">
       <img
-        className="recent-team-img"
         src={competingTeamLogo}
+        className="competing-team-logo"
         alt={`competing team ${competingTeam}`}
       />
-      <p className="recent-team-name">{competingTeam}</p>
+      <p className="competing-team-name">{competingTeam}</p>
       <p className="result">{result}</p>
-      <p className={`result-status ${statusColor}`}>{matchStatus}</p>
+      <p className={matchStatusClassName}>{matchStatus}</p>
     </li>
   )
 }
